bc051195942670901ac38d9db51147d88f412914
Nicolas Kaufmann <Mr.Spock.NK@gmx.de>
2016-11-29 21:55:06 +0100

    Changed code to work with MySensors V2.0 * Code Cleanups / formating

--- RoomSensor/RoomSensor.ino
+++ RoomSensor/RoomSensor.ino
@@ -1,22 +1,40 @@
-#include <MySensor.h>
+// MySensors Config
+// Enable debug prints to serial monitor
+// #define MY_DEBUG 
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+#define MY_RF24_CE_PIN 9    // Radio specific settings for RF24
+#define MY_RF24_CS_PIN 10   // Radio specific settings for RF24 
+
+// ------- Includes -------
+#include <MySensors.h>
 #include "arduino.h"
 #include <DHT.h>
 #include "SparkFunBME280.h"
 #include <SI7021.h>
 
-//configuration
-#define HAS_NTC
-//#define HAS_LDR
-//#define HAS_BME280
-#define HAS_SI7021
-#define HAS_DHT
+// -------  Configuration Start ------- 
+  // Sensors configuration:
+  // Comment in all sensors connected:
+  #define HAS_NTC
+  //#define HAS_LDR
+  //#define HAS_BME280
+  #define HAS_SI7021
+  #define HAS_DHT
 
-//BME280 related:
+  // How long should the sensor sleep ?
+  //#define SLEEP_TIME (2UL*60UL*1000UL)    // 2min * 60sec * 1000ms
+  #define SLEEP_TIME (2UL*1000UL)           // 2sec * 1000ms
+
+// -------  Configuration End -------
+
+// BME280 related:
 BME280 bmeSensor;
 
-//SI7021
+// SI7021 related:
 SI7021 SI7021Sensor;
-
 
 // DHT Sensor related:
 #define HUMIDITY_SENSOR_DIGITAL_PIN 3
@@ -34,25 +52,26 @@ boolean metric = true;
 #define SI_Temp_ID 7
 #define SI_Humidity_ID 8
 
-// mysensors related
-MySensor gw;
+// mysensors related:
 //MyMessage msg(Moisture_ID,V_LEVEL);
-MyMessage DHTTempMsg(DHT_Temp_ID,V_TEMP);
-MyMessage DHTHumMsg(DHT_Humidity_ID,V_HUM);
+#ifdef HAS_DHT
+  MyMessage DHTTempMsg(DHT_Temp_ID,V_TEMP);
+  MyMessage DHTHumMsg(DHT_Humidity_ID,V_HUM);
+#endif 
 #ifdef HAS_NTC
-MyMessage NTCTempMsg(NTC_Temp_ID,V_TEMP);
+  MyMessage NTCTempMsg(NTC_Temp_ID,V_TEMP);
 #endif
 #ifdef HAS_LDR
-MyMessage LDRLightMsg(LDR_Light_ID,V_LIGHT_LEVEL);
+  MyMessage LDRLightMsg(LDR_Light_ID,V_LIGHT_LEVEL);
 #endif
 #ifdef HAS_BME280
-MyMessage BMETempMsg(BME_Temp_ID,V_TEMP);
-MyMessage BMEHumMsg(BME_Humidity_ID,V_HUM);
-MyMessage BMEBaroMsg(BME_Pressure_ID,V_PRESSURE);
+  MyMessage BMETempMsg(BME_Temp_ID,V_TEMP);
+  MyMessage BMEHumMsg(BME_Humidity_ID,V_HUM);
+  MyMessage BMEBaroMsg(BME_Pressure_ID,V_PRESSURE);
 #endif
 #ifdef HAS_SI7021
-MyMessage SITempMsg(SI_Temp_ID,V_TEMP);
-MyMessage SIHumMsg(SI_Humidity_ID,V_HUM);
+  MyMessage SITempMsg(SI_Temp_ID,V_TEMP);
+  MyMessage SIHumMsg(SI_Humidity_ID,V_HUM);
 #endif
 
 
@@ -62,71 +81,46 @@ unsigned long timediff(unsigned long t1, unsigned long t2);
 int NTC_ADC2Temperature(unsigned int adc_value);
 #endif
 int getBatteryLevel(void);
-
-
 extern volatile unsigned long timer0_millis;
 
-void setup() {
-  // put your setup code here, to run once:
+void before() {
+  // run code before mysensors init
   Serial.begin(115200);
   while(!Serial)
   {
    ; // wait for serial port to connect. Needed for native USB
   }
   Serial.print("Serial init\r\n"); 
-
-  //pinMode(A3,INPUT);
-  //while(1)
-  //{
-  //  getBatteryLevel();
-  //  delay(200);
-  //}
-
-  // mysensors init:
-  gw.begin();
-  Serial.print("GW Begin\r\n");
-  gw.sendSketchInfo("Room Sensor Node", "1.0"); 
-  Serial.print("Sketch Info\r\n");
   
-  //register the sensors to the gateway
-  gw.present(DHT_Temp_ID , S_TEMP);
-  gw.present(DHT_Humidity_ID , S_HUM);
-  #ifdef HAS_NTC
-  gw.present(NTC_Temp_ID , S_TEMP);
-  #endif
-  #ifdef HAS_LDR
-  gw.present(LDR_Light_ID , S_LIGHT_LEVEL);
-  #endif
-  #ifdef HAS_BME280
-  gw.present(BME_Temp_ID , S_TEMP);
-  gw.present(BME_Humidity_ID , S_HUM);
-  gw.present(BME_Pressure_ID , S_BARO);
-  #endif
+}
 
-  #ifdef HAS_SI7021
-  gw.present(SI_Temp_ID , S_TEMP);
-  gw.present(SI_Humidity_ID , S_HUM);
-  #endif
+void setup() {
+  // put your setup code here, to run once:
+
+  // mysensors presentation:
+  presentation();
   
-  
+#ifdef HAS_DHT
   // ------ DHT11 init -----
   //Pin 4 is the power source for the DHT
   pinMode(4,OUTPUT);
   digitalWrite(4,HIGH);
-  //
-  //gw.sleep(2*1000); // 2 sec
+  // 
   dht.setup(HUMIDITY_SENSOR_DIGITAL_PIN); 
   dht.getTemperature();
   delay(2000);
   Serial.print("DHT init\r\n");
+#endif
 
+#if defined(HAS_NTC) || defined(HAS_LDR)
   // ---- NTC / LDR Init ----
   pinMode(A1,INPUT);
   pinMode(A2,OUTPUT);
   digitalWrite(A2,LOW);
   Serial.print("NTC/LDR init\r\n");
+#endif
 
-  #ifdef HAS_BME280
+#ifdef HAS_BME280
   // ------ BME280 Init ------
   bmeSensor.settings.commInterface = I2C_MODE;
   bmeSensor.settings.I2CAddress = 0x77;
@@ -150,161 +144,164 @@ void setup() {
   Serial.print("\n\n");
   
   Serial.print("BME280 init\r\n"); 
-  #endif
+#endif
 
   
-  #ifdef HAS_SI7021
+#ifdef HAS_SI7021
   SI7021Sensor.begin();
-  #endif
+  Serial.println("SI7021 init"); 
+#endif
   
   //  ---- Battery Input ----
   pinMode(A3,INPUT);
   
-}
+} // END void setup() 
 
-
-void loop() {
+void presentation()  
+{
+  sendSketchInfo("Room Sensor Node", "1.1"); 
+  Serial.println("Sketch Info");
   
-  static uint8_t DoSleep = 1;
+  //register the sensors to the gateway
+  #ifdef HAS_DHT
+  present(DHT_Temp_ID , S_TEMP);
+  present(DHT_Humidity_ID , S_HUM);
+  #endif
 
-  // Testloop:
-  /*  
-  while(0)
-  {
-    si7021_env data = SI7021Sensor.getHumidityAndTemperature();
-    Serial.print("Temp: ");
-    Serial.println(data.celsiusHundredths);
-    Serial.print("HUM: ");
-    Serial.println(data.humidityPercent);
-
-    delay(5000);
-    
-  }
-  */
+  #ifdef HAS_NTC
+  present(NTC_Temp_ID , S_TEMP);
+  #endif
   
-  // static unsigned long TempTime = 0;
-  // if (timediff(TempTime, millis()) > 4000 )
-  {
-    
-      digitalWrite(A2, HIGH);
-     // TempTime = millis();
-
-    #ifdef HAS_NTC
-      Serial.println("ntc:");
-      int ntc = analogRead(A1);
-      int temp = NTC_ADC2Temperature(ntc);
-      Serial.println(ntc);
-      Serial.println(temp);
-      //send the measured value
-      gw.send(NTCTempMsg.set(((float)temp)/10,1));
-    #endif
-    #ifdef HAS_LDR
-      Serial.println("ldr:");
-      int ldr = analogRead(A1);
-      Serial.println(ldr);
-      //send the measured value
-      gw.send(LDRLightMsg.set(ldr, 1) );
-    #endif
-      
-      digitalWrite(A2, LOW);
-      Serial.println();
-      gw.sendBatteryLevel(getBatteryLevel());
-      Serial.println();
-  }
-  // dht test:
-  //static unsigned long DHTTime = 0;
-  //if(timediff(millis(), DHTTime) > 4000) // every ~ 2sec
-  {
-    //DHTTime = millis();
-    float temperature = dht.getTemperature();
-    if (isnan(temperature)) {
-      Serial.println("Failed reading temperature from DHT");
-    }else{
-      if (!metric) {
-        temperature = dht.toFahrenheit(temperature);
-      }
-      gw.send(DHTTempMsg.set(temperature, 1));
-      Serial.println();
-      Serial.print("DHT Temp: ");
-      Serial.println(temperature); 
-    }
-    float humidity = dht.getHumidity();
-    if (isnan(humidity)) {
-        Serial.println("Failed reading humidity from DHT");
-    }
-    else{
-      gw.send(DHTHumMsg.set(humidity, 1));
-      Serial.println();
-      Serial.print("DHT Hum: ");
-      Serial.println(humidity);
-    }
-  }
-
+  #ifdef HAS_LDR
+  present(LDR_Light_ID , S_LIGHT_LEVEL);
+  #endif
+  
   #ifdef HAS_BME280
-  //static unsigned long BMETime = 0;
-  //if(timediff(millis(), BMETime) > 4000) // every ~ 4sec
-  {
-    //BMETime=millis();
-  
-    float temp = bmeSensor.readTempC(); 
-    gw.send(BMETempMsg.set(temp, 1));
-    Serial.println();
-    Serial.print("Temperature: ");
-    Serial.print(temp, 2);
-    Serial.println(" degrees C");
-   
-    Serial.print("Temperature: ");
-    Serial.print(bmeSensor.readTempF(), 2);
-    Serial.println(" degrees F"); 
-      
-    float pressure = bmeSensor.readFloatPressure();
-    gw.send(BMEBaroMsg.set(pressure, 1));
-    Serial.println();
-    Serial.print("Pressure: ");
-    Serial.print(pressure, 2);
-    Serial.println(" Pa");
-      
-    Serial.print("Altitude: ");
-    Serial.print(bmeSensor.readFloatAltitudeMeters(), 2);
-    Serial.println("m");
-  
-    Serial.print("Altitude: ");
-    Serial.print(bmeSensor.readFloatAltitudeFeet(), 2);
-    Serial.println("ft"); 
-  
-    float humidity = bmeSensor.readFloatHumidity();
-    gw.send(BMEHumMsg.set(humidity, 1));
-    Serial.println();
-    Serial.print("%RH: ");
-    Serial.print(humidity, 2);
-    Serial.println(" %");
-    
-    Serial.println();
-  }
+  present(BME_Temp_ID , S_TEMP);
+  present(BME_Humidity_ID , S_HUM);
+  present(BME_Pressure_ID , S_BARO);
   #endif
 
   #ifdef HAS_SI7021
-    si7021_env data = SI7021Sensor.getHumidityAndTemperature();
-    float temp = ((float)data.celsiusHundredths) / 100; 
-    gw.send(SITempMsg.set(temp, 1));
-    
-    Serial.print("SI Temp: ");
-    Serial.println(temp);
-    
-    float humidity = data.humidityPercent;
-    gw.send(SIHumMsg.set(humidity, 1));
-    Serial.print("SI HUM: ");
-    Serial.println(humidity);
+  present(SI_Temp_ID , S_TEMP);
+  present(SI_Humidity_ID , S_HUM);
   #endif
-
   
-  //#define SLEEP_TIME (2UL*60UL*1000UL)    // 2min * 60sec * 1000ms
-  #define SLEEP_TIME (2UL*1000UL)    // 2sec * 1000ms
-  
-  //go into power down mode: 
-  gw.sleep(SLEEP_TIME);  
+} // END void presentation()  
 
-  // adjust the value for millis():
+void loop() {
+  float temp;
+  float humidity;
+  float pressure;
+
+  Serial.println("------ Loop begin -------");
+
+  // NTC | LDR
+  digitalWrite(A2, HIGH);   // Power ON NTC / LDR
+
+#ifdef HAS_NTC
+  int ntc = analogRead(A1);
+  temp = NTC_ADC2Temperature(ntc)/10;
+  Serial.print("ntc adc: ");
+  Serial.println(ntc);
+  Serial.print("ntc Temp: ");
+  Serial.println(temp);
+  //send the measured value
+  send(NTCTempMsg.set(temp,1));
+#endif
+#ifdef HAS_LDR
+  Serial.println("ldr:");
+  int ldr = analogRead(A1);
+  Serial.println(ldr);
+  //send the measured value
+  send(LDRLightMsg.set(ldr, 1) );
+#endif
+  
+  digitalWrite(A2, LOW);  // Power OFF NTC / LDR
+
+  // Send Battery Level:
+  sendBatteryLevel(getBatteryLevel());
+  
+  // DHT
+#ifdef HAS_DHT
+  temp = dht.getTemperature();
+  if (isnan(temp)) {
+    Serial.println("Failed reading temperature from DHT");
+  }else{
+    if (!metric) {
+      temp = dht.toFahrenheit(temp);
+    }
+    send(DHTTempMsg.set(temp, 1));
+    Serial.print("DHT Temp: ");
+    Serial.println(temp); 
+  }
+  humidity = dht.getHumidity();
+  if (isnan(humidity)) {
+      Serial.println("Failed reading humidity from DHT");
+  }
+  else{
+    send(DHTHumMsg.set(humidity, 1));
+    Serial.print("DHT Hum: ");
+    Serial.println(humidity);
+  }
+#endif
+
+  // BME280
+#ifdef HAS_BME280
+  {
+  temp = bmeSensor.readTempC(); 
+  send(BMETempMsg.set(temp, 1));
+  Serial.print("Temperature: ");
+  Serial.print(temp, 2);
+  Serial.println(" degrees C");
+ 
+  Serial.print("Temperature: ");
+  Serial.print(bmeSensor.readTempF(), 2);
+  Serial.println(" degrees F"); 
+    
+  pressure = bmeSensor.readFloatPressure();
+  send(BMEBaroMsg.set(pressure, 1));
+  Serial.print("Pressure: ");
+  Serial.print(pressure, 2);
+  Serial.println(" Pa");
+    
+  Serial.print("Altitude: ");
+  Serial.print(bmeSensor.readFloatAltitudeMeters(), 2);
+  Serial.println("m");
+
+  Serial.print("Altitude: ");
+  Serial.print(bmeSensor.readFloatAltitudeFeet(), 2);
+  Serial.println("ft"); 
+
+  humidity = bmeSensor.readFloatHumidity();
+  send(BMEHumMsg.set(humidity, 1));
+  Serial.print("%RH: ");
+  Serial.print(humidity, 2);
+  Serial.println(" %");
+  
+  Serial.println();
+  }
+#endif
+
+  // SI7021
+#ifdef HAS_SI7021
+  si7021_env data = SI7021Sensor.getHumidityAndTemperature();
+  temp = ((float)data.celsiusHundredths) / 100; 
+  send(SITempMsg.set(temp, 1));
+  Serial.print("SI Temp: ");
+  Serial.println(temp);
+  
+  humidity = data.humidityPercent;
+  send(SIHumMsg.set(humidity, 1));
+  Serial.print("SI HUM: ");
+  Serial.println(humidity);
+#endif
+
+  // ----- go into power down mode: -----
+  Serial.println("Go to Sleep !");
+  sleep(SLEEP_TIME);  
+
+  // adjust the value for millis() after sleep:
   uint8_t oldSREG = SREG;
   cli();
   timer0_millis += SLEEP_TIME;  
